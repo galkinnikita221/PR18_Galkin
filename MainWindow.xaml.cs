5630f8f4e007a638b1f9cbb6d260479a70a4d8c9
student-a310 <student-a310@PC-A310-9>
2023-11-23 11:26:12 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrintManagementSystem_Galkin
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Delete_Operation(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Edit_Operation(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void SelectedType(object sender, SelectionChangedEventArgs e)
+		{
+
+		}
+
+		private void SelectedFormat(object sender, SelectionChangedEventArgs e)
+		{
+
+		}
+
+		private void textBoxCount_Changed(object sender, TextChangedEventArgs e)
+		{
+
+		}
+
+		private void AddOperation(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void ColorsChange(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+		{
+
+		}
+	}
+}
